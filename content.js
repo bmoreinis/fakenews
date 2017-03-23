abe18c6d08b65a470a25344c3b553354776c52e6
Bram Moreinis <bmoreinis@gmail.com>
2017-03-23 16:21:39 -0400

    Simplified; sent to test.fakenewsfitness.org;
    Removed many extraneous lines to simplify this for our purposes.

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 function sendToServer(obj) {
     console.log(obj);
 
-    fetch('http://app.fakenewsfitness.org', {
+    fetch('http://test.fakenewsfitness.org', {
         method: 'POST',
         body: JSON.stringify(obj)
     })
@@ -48,86 +48,12 @@ var controller = (function(){
    function getURL() {
      var href = window.location.href.split('?');
      return href[0];	 
-   };
-   
-   function domainFinder() {
-	var myPath = window.location.host.split('.');
-	if (myPath.length === 3) {
-		return myPath[1]+"."+myPath[2]
-	}
-	else {
-		return myPath[0]+"."+myPath[1]
-	};
-  };
-
-  // extract tld from url of active tab
-  function tldParser() {
-	var myPath = window.location.host.split('.');
-	if (myPath.length === 3) {
-		return myPath[2]
-	}
-	else {
-		return myPath[1]
-	};
-  };
-
-  // search document.body for ABOUT US or similar in menu lists
-  function aboutFinder () {
-	//insert fxn here
-  };
-
-  // search document.body for posted date above or below body
-  function dateFinder () {
-	var modifiedDate = new Date(document.lastModified);
-	var today = new Date();
-    //Check for dynamically created page
-	if (String(modifiedDate) !== String(today)) {
-		var splitDate = String(modifiedDate).split(' ');
-		var returnDate = splitDate[0]+' '+splitDate[1]+' '+splitDate[2]+' '+splitDate[3];
-		return returnDate;
-	}
-	//If modifiedDate doesn't work, search page for updated, posted, revised, modified + date
-	else {
-		var bodyTag = document.getElementsByTagName("body");
-		var bodyDates = bodyTag[0].innerText.match(/((((U|u)pdated)|((P|p)osted)|((R|r)evised)|((M|m)odified))( |: | on )(\d{1,2})(-|\/)(\d{1,2})(-|\/)(\d{4}))|(\d{1,2})(-|\/)(\d{1,2})(-|\/)(\d{4})/);
-		//Check that we found anything
-		if (bodyDates) {
-		return bodyDates.toString();
-		}
-		else {
-		return 'We could not find anything on the page';
-		};
-	};
-  };
-
-  // lists all links within body of page
-  function linkFinder () {
-	var array = [];
-	var paragraphs = document.getElementsByTagName("p");
-		for(var i=0, max=paragraphs.length; i<max; i++) {
-			for(var x=0, Xmax=paragraphs[i].children.length; x<Xmax; x++) {
-				if (paragraphs[i].children[x].tagName == "A") {
-					array.push(paragraphs[i].children[x].href)
-				}
-			}
-		}
-	return array;
-  };
-
-  // does something like http://smallseotools.com/backlink-checker/
-  function backlinkCounter () {
-	//insert fxn here
-  }
+   }; 
 
   //return public methods
   return {
-	getURL : getURL,
-	domainFinder : domainFinder,
-    tldParser : tldParser,
-	linkFinder : linkFinder,
-	dateFinder : dateFinder
+	getURL : getURL
   };
-
 })();
 
 //cancel button - now that we have multiple pages could/should be made generic
@@ -146,135 +72,45 @@ function addLink() {
 	divElement.appendChild(newElement);
 };
 
-function makeForm(fields, critFields) {
-	// Move Body Down
-    document.getElementsByTagName("BODY")[0].style.marginTop="420px";
+function makeForm(fields) {
+    // Move Body Down
+	document.getElementsByTagName("BODY")[0].style.marginTop="420px";
+
     // Create Form Object Page 1
 	var formDiv = document.createElement("div");
 	formDiv.setAttribute('id', "FakeNewsForm");
-    var formName = document.createElement("form");
-    for(var i = 0; i < fields.length; i++){
-        var labelElement = document.createElement("label");
-        labelElement.setAttribute("for", fields[i][0]);
-        var labelText = document.createTextNode(fields[i][1]+": ");
-        labelElement.appendChild(labelText);
-        formName.appendChild(labelElement);
-        switch(fields[i][3]) {
-        	case "a":
-                    var divElement = document.createElement("div");
-                        divElement.setAttribute('id','blankLinks');
-                    var newLink = document.createElement("input");
-                        newLink.setAttribute('type',"button");
-                        newLink.setAttribute('value',"Add New URL");
-                        newLink.setAttribute('class','newField');
-                        newLink.addEventListener('click', addLink, false);
-                        divElement.appendChild(newLink);
-                        formName.appendChild(divElement);
-                    var inputElement = document.createElement("input"); //input element, text
-                        inputElement.setAttribute('type',"text");
-                        inputElement.setAttribute("id",fields[i][0]);
-                        inputElement.setAttribute('class','emptyField');
-                        inputElement.value = "[Begin with http://]";
-                        divElement.appendChild(inputElement);
-                    break;
-        	case "v":
-                        if (fields[i][2].length > 0) {
-                                var listNode = document.createElement("OL");
-                                listNode.setAttribute("id", fields[i][0]);
-                                var itemsArray = fields[i][2];
-                                for(var x = 0; x < itemsArray.length; x++){
-                                        var listItem = document.createElement("LI"); // Create a <li> node
-                                        var listText = document.createTextNode(itemsArray[x]);// Create a text node
-                                        listItem.appendChild(listText);
-                                        listNode.appendChild(listItem);
-                                }
-                                formName.appendChild(listNode);
-                        }
-                        else {
-                                var listNode = document.createElement("UL");
-                                listNode.setAttribute("id", fields[i][0]);
-                                var listItem = document.createElement("LI");
-                                var listText = document.createTextNode('No items were found');
-                                listItem.appendChild(listText);
-                                listNode.appendChild(listItem);
-                                formName.appendChild(listNode);
-                        }
-                        break;
-                case "vl":
-                    if (fields[i][2].length > 0) {
-                            var listNode = document.createElement("OL");
-                            listNode.setAttribute("id", fields[i][0]);
-                            var itemsArray = fields[i][2];
-                            for(var x = 0; x < itemsArray.length; x++){
-                                    var listItem = document.createElement("LI"); // Create a <li> node
-                                    var itemLink = document.createElement('a')
-                                    var linkText = document.createTextNode(itemsArray[x]);// Create a text node
-                                    itemLink.appendChild(linkText);
-                                    itemLink.href = itemsArray[x];
-                                    itemLink.setAttribute('target', '_blank');
-                                    listItem.appendChild(itemLink);
-                                    listNode.appendChild(listItem);
-                            }
-                            formName.appendChild(listNode);
-                    }
-                    else {
-                            var listNode = document.createElement("UL");
-                            listNode.setAttribute("id", fields[i][0]);
-                            var listItem = document.createElement("LI");
-                            var listText = document.createTextNode('No items were found');
-                            listItem.appendChild(listText);
-                            listNode.appendChild(listItem);
-                            formName.appendChild(listNode);
-                    }
-                    break;
-        	case "f":
-		        var inputElement = document.createElement("input"); //input element, text
-		        inputElement.setAttribute('type',"text");
-		        inputElement.setAttribute("id",fields[i][0]);
-		        inputElement.value = fields[i][2];
-		        formName.appendChild(inputElement);
-		    	break;
+    	var formName = document.createElement("form");
+
+    // Create Input Elements	
+    	for(var i = 0; i < fields.length; i++){
+		var labelElement = document.createElement("label");
+		labelElement.setAttribute("for", fields[i][0]);
+		var labelText = document.createTextNode(fields[i][1]+": ");
+		labelElement.appendChild(labelText);
+		formName.appendChild(labelElement);
+		var inputElement = document.createElement("input"); //input element, text
+		inputElement.setAttribute('type',"text");
+		inputElement.setAttribute("id",fields[i][0]);
+		inputElement.value = fields[i][2];
+		formName.appendChild(inputElement);
+    	}
+	
+     // Create Submit Element	
+	var submitElement = document.createElement("input"); //input element, Submit button
+	submitElement.setAttribute('type',"button");
+	submitElement.setAttribute('value',"Submit Data");
+	submitElement.setAttribute('id',"submit");
+	submitElement.addEventListener("click", function() {
+		sendToServer({
+		/* use Jquery with a form serialization library */
+		username: document.getElementById("username").value
+		})
+		}, false)
+	formName.appendChild(submitElement);
+	if (document.getElementById('FakeNewsForm')) {
+		document.getElementById('FakeNewsForm').replaceWith(formDiv);
 		}
-    }
-	
-    var submitElement = document.createElement("input"); //input element, Submit button
-    submitElement.setAttribute('type',"button");
-    submitElement.setAttribute('value',"Submit Data");
-    submitElement.setAttribute('id',"submit");
-    submitElement.addEventListener("click", function() {
-        sendToServer({
-        /* use Jquery with a form serialization library */
-        username: document.getElementById("username").value
-        })
-    }, false)
-    formName.appendChild(submitElement);
-
-	var cancelElement = document.createElement("input"); //input element, cancel
-	cancelElement.setAttribute('type',"button");
-	cancelElement.setAttribute('value',"Cancel (You will lose your work)");
-	cancelElement.setAttribute('id','cancel');
-	cancelElement.addEventListener('click', cancelForm, false);
-        formName.appendChild(cancelElement);
-	formDiv.appendChild(formName);
-	
-
-	var submitAllElement = document.createElement("input"); //input element, Submit All button
-        submitAllElement.setAttribute('type',"button");
-        submitAllElement.setAttribute('value',"Submit All Data");
-	submitAllElement.setAttribute('id',"submitAll");
-        submitAllElement.addEventListener("click", function() {
-        sendToServer({
-        /* use Jquery with a form serialization library */
-            username: document.getElementById("username").value
-        })
-    }, false)
-    
-  
-	
-    if (document.getElementById('FakeNewsForm')) {
-        document.getElementById('FakeNewsForm').replaceWith(formDiv);
-    }
-    else {
-        document.getElementsByTagName('body')[0].appendChild(formDiv);
-    };
+		else {
+		document.getElementsByTagName('body')[0].appendChild(formDiv);
+	};
 };
